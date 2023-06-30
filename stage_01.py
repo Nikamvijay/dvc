0cf451b9f90e770231170877a929a0abb8fd5a39
Nikamvijay <vijaynikam739@gmail.com>
2023-06-30 22:26:02 +0530

    Merge branch 'master' of https://github.com/Nikamvijay/dvc

---
+++ stage_01.py
@@ -0,0 +1,3 @@
+
+with open('artifact.txt','w') as f:
+    f.write('my name is vijay stage one is done')
---
+++ stage_02.py
@@ -0,0 +1,6 @@
+
+with open('artifact.txt','r') as f:
+    text=f.read()
+
+print(text)
+print('this the stage-02 file')
